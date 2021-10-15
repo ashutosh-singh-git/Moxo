1f512fae12970b8984fd80f6ce0e7674ce05bf67
ashutosh-wynk <ashutosh.singh5335@gmail.com>
2021-10-15 18:54:53 +0530

    Added login info to README.md

--- src/main/java/com/moxo/app/entity/ConfigEntity.java
+++ src/main/java/com/moxo/app/entity/ConfigEntity.java
@@ -1,14 +1,9 @@
 package com.moxo.app.entity;
 
-import com.moxo.app.dto.ConfigType;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.Version;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "config")
